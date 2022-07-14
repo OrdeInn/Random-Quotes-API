e7f43cba5a065cd694ca94fb827c9ac6af75f06d
OrdeInn <emiroguz98@gmail.com>
2022-07-14 23:57:07 +0300

    implemented auth controllers

--- controllers/user.go
+++ controllers/user.go
@@ -3,16 +3,23 @@ package controllers
 import (
 	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"orderinn.com/random-quotes/configs"
 	"orderinn.com/random-quotes/models"
 	"orderinn.com/random-quotes/responses"
+	token "orderinn.com/random-quotes/utils"
 	"time"
 )
 
 type RegisterInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginInput struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
@@ -75,6 +82,58 @@ func Register() gin.HandlerFunc {
 
 func Login() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var input LoginInput
+
+		//Validate input
+		err := c.ShouldBindJSON(&input)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		//Query the user from collection
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		var user models.User
+
+		err = userCollection.FindOne(ctx, bson.M{"username": input.Username}).Decode(&user)
+
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		//Validate password
+		err = verifyPassword(&user, input.Password)
+
+		if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			return
+		}
+
+		userToken, err := token.GenerateToken(user.Id)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, responses.UserResponse{
+			Status:  http.StatusOK,
+			Message: "success",
+			Data: map[string]interface{}{
+				"data": map[string]interface{}{
+					"username": user.Username,
+					"token":    userToken,
+				},
+			},
+		})
 
 	}
+}
+
+func verifyPassword(user *models.User, inputPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(inputPassword))
 }
